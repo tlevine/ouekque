462c4fbf5d23c394fe6b7da64de4f62df99b145e
Thomas Levine <_@thomaslevine.com>
2014-07-18 05:54:48 -0400

    -

--- Ouekque.hs
+++ Ouekque.hs
@@ -10,6 +10,7 @@ dress (Sort parent func) x = case (dress parent x) of
 anySort :: Sort a
 anySort = Any $ id . Just
 
--- wholeNumber = Sort anySort (\x -> if x >= 0 then Just x else Nothing)
+wholeNumber :: Sort Integral
+wholeNumber = Sort anySort (\x -> if x >= 0 then Just x else Nothing)
 
 main = do putStr "h"

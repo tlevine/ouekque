6775066d8ee97d713dae1d372ddea726666e0e11
Thomas Levine <_@thomaslevine.com>
2014-07-18 06:05:40 -0400

    -

--- Ouekque.hs
+++ Ouekque.hs
@@ -1,10 +1,16 @@
 data Sort a = Any           (a -> Maybe a)
             | Sort (Sort a) (a -> Maybe a)
+            | Join (Sort a) (Sort a)
 
 dress :: Sort a -> a -> Maybe a
 dress (Any         func) x = func x
 dress (Sort parent func) x = case (dress parent x) of
   Just p  -> func p
+  Nothing -> Nothing
+dress (Join (Sort mother motherFunc) (Sort father fatherFunc)) x = case (dress mother x) of
+  Just y  -> case (dress father $ motherFunc y) of
+    Just z -> fatherFunc z
+    Nothing -> Nothing
   Nothing -> Nothing
 
 anySort :: Sort a
@@ -14,12 +20,17 @@ anySort = Any $ id . Just
 
 -- Some sorts!
 -- Move these to a separate file eventually
-wholeNumber :: Sort Int
-wholeNumber = Sort anySort (\x -> if x >= 0 then Just x else Nothing)
+integer :: Sort Int
+integer = Sort anySort id
+
+positiveInteger :: Sort Int
+positiveInteger = Sort integer $ \x -> if x >= 0 then Just x else Nothing
 
 evenNumber :: Sort Int
-evenNumber = Sort anySort (\x -> if (even x) then Just x else Nothing)
+evenNumber = Sort integer $ \x -> if (even x) then Just x else Nothing
 
+na :: Sort String
+na = Sort String $ \x -> if (x == "NA") then Just x else Nothing
 
 
 
